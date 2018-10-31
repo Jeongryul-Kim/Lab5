fcf81d5b84a48653f5cf6003014916994a5d943c
Jeongryul Kim <cs15xrr@its-cseb250-10.ucsd.edu>
2018-10-31 15:40:26 -0700

    Add an author/email comment

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello
 {
     public static void main( String argv[] )
     {
+	//Author: Jeongryul Kim (jek128@ucsd.edu)
 	//Default is "World"
         String name = "World";
         if ( argv.length != 0 )
